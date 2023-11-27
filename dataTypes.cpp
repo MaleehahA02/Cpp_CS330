11669f418062873d29381add68ee9b5e3a5b387e
MaleehahA02 <144168274+MaleehahA02@users.noreply.github.com>
2023-11-27 13:31:07 -0500

    Create dataTypes.cpp

---
+++ dataTypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main () {
+  int a = 25;               
+  float b = 7.50;     
+  double c = 10.25;   
+  char d = 'M';         
+  bool e = true;       
+  string f = "My name is";   
+   
+  // To print the values of the variables
+  cout << "int: " << a << "\n";
+  cout << "float: " << b << "\n";
+  cout << "double: " << c << "\n";
+  cout << "char: " << d << "\n";
+  cout << "bool: " << e << "\n";
+  cout << "string: " << f << "\n";
+ 
+  return 0;
+}
