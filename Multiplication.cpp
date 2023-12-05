9a9ba8d8e5ff26dfb7478db158e191c6ae609c2a
MaleehahA02 <144168274+MaleehahA02@users.noreply.github.com>
2023-12-05 14:39:32 -0500

    Create Multiplication.cpp

---
+++ Multiplication.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void myMultiplication(int num1, int num2) {
+  int product = num1 * num2;
+  cout << product <<"\n";
+}
+
+int main() {
+  myMultiplication(3,2);
+  myMultiplication(5,10);
+  return 0;
+}
+
+//Output
+6
+5
