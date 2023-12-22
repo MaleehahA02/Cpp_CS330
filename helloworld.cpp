5f45dbb5c7a11f78231927f3d0e98497aa92d459
MaleehahA02 <144168274+MaleehahA02@users.noreply.github.com>
2023-12-22 02:02:08 -0500

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,9 +4,7 @@ int main() { // main function
 
     std::cout << "Hello World!"; //print function
     
-
     return 0; // exit function
-    //The main() function returns 0 if the program was successfully executed
 } 
 
 
