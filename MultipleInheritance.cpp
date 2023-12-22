bd0557cb78e4b4ba5b6a21ce4d93676e512cb457
MaleehahA02 <144168274+MaleehahA02@users.noreply.github.com>
2023-12-22 05:57:25 -0500

    Create MultipleInheritance.cpp

---
+++ MultipleInheritance.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+using namespace std;
+    
+    // First Parent (Base) Class
+class Person {
+  public:
+    string name;
+    int age;
+    };
+    
+    // Second Parent (Base) Class
+class Event {
+  public:
+    string location;
+    string date;
+    string time;
+};
+    
+    // Child (Derived) Class - Inherits from both the Person and Event classes
+class Appointment : public Person, public Event {
+};
+
+int main() {
+  Appointment appt1;
+  appt1.name = "John";
+  appt1.age = 45;
+  appt1.location = "Health Check-Up";
+  appt1.date = "2023-10-21";
+  appt1.time = "10:00 AM";
+
+  Appointment appt2;
+  appt2.name = "Jane";
+  appt2.age = 35;
+  appt2.location = "Conference Room";
+  appt2.date = "2023-01-24";
+  appt2.time = "3:00 PM";
+
+  cout << "Appointment 1:\n";
+  cout << "Name: " << appt1.name << "\n";
+  cout << "Age: " << appt1.age << "\n";
+  cout << "Location: " << appt1.location << "\n";
+  cout << "Date: " << appt1.date << "\n";
+  cout << "Time: " << appt1.time << "\n\n";
+
+  cout << "Appointment 2:\n";
+  cout << "Name: " << appt2.name << "\n";
+  cout << "Age: " << appt2.age << "\n";
+  cout << "Location: " << appt2.location << "\n";
+  cout << "Date: " << appt2.date << "\n";
+  cout << "Time: " << appt2.time << "\n";
+
+  return 0;
+}
+
+
+//Output:
+
+/*    
+    Appointment 1:
+    Name: John
+    Age: 45
+    Location: Health Check-Up
+    Date: 2023-10-21
+    Time: 10:00 AM
+    
+    Appointment 2:
+    Name: Jane
+    Age: 35
+    Location: Conference Room
+    Date: 2023-01-24
+    Time: 3:00 PM
+  */
