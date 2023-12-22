ff6dbbe2bf5c4076824368b67d539d4964f77647
MaleehahA02 <144168274+MaleehahA02@users.noreply.github.com>
2023-12-22 01:55:15 -0500

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
-int main() {
-    std::cout << "Hello World!";
-    return 0;}
+#include <iostream> // header file library to input and output objects
+
+int main() { // main function
+
+    std::cout << "Hello World!"; //print function
+    
+
+    return 0; // exit function
+    //The main() function returns 0 if the program was successfully executed
+} 
+
 
 /*
 Output:
